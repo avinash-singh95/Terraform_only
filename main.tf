80a50298f442f1dd6a071ec347e07076124fd97b
avinash <avinashDemo@outlook.com>
2021-09-13 13:44:34 +0530

    using default modile

--- main.tf
+++ main.tf
@@ -1,35 +1,48 @@
+terraform {
+  backend "s3" {
+    bucket = "serverbucket1"
+    key    = "avi/backendbucket"
+    region = "ap-south-1"
+  }
+}
+
+
+
 provider "aws" {
     region = "ap-south-1"
 }
 
 
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  name = "my-vpc"
+  cidr = var.vpn_cidr_block
 
-resource "aws_vpc" "myapp-vpc" {
-     cidr_block = var.vpn_cidr_block
-     tags = {
-         Name: "${var.env_prefix}-vpc"     }
+  azs             = [var.avail_zone]
+  public_subnets  = [var.subnet_cidr_block]
+
+
+  tags = {
+    Name: "${var.env_prefix}-vpc" 
+  }
+
+  public_subnet_tags = {
+      Name = "${var.env_prefix}-subnet" 
+  }
 }
 
-module "myapp-subnet" {
-    source = "./modules/subnets"
-    subnet_cidr_block = var.subnet_cidr_block
-    avail_zone = var.avail_zone
-    env_prefix = var.env_prefix
-    vpc_id = aws_vpc.myapp-vpc.id
-    default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
 
 
-}
 
 
 module "myapp-server" {
     source = "./modules/webserver"
-    vpc_id = aws_vpc.myapp-vpc.id
+    vpc_id = module.vpc.vpc_id
     my_ip = var.my_ip
     env_prefix = var.env_prefix
     image_name = var.image_name
     instance_type = var.instance_type
-    subnet_id = module.myapp-subnet.subnet.id
+    subnet_id = module.vpc.public_subnets[0]
     avail_zone = var.avail_zone
 
 
--- modules/subnets/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "subnet" {
-    value = aws_subnet.dev-subnet-1
-}
